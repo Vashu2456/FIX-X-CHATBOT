156cd15b0852ba9157a3cacdc0857f2f3dc83889
SKD-VASHU <165390858+Vashu2456@users.noreply.github.com>
2024-05-30 07:02:02 +0530

    Create __init__.py

---
+++ VASHU X CHATBOT/database/__init__.py
@@ -0,0 +1,10 @@
+from pymongo import MongoClient
+
+import config
+
+anniedb = MongoClient(config.MONGO_URL)
+Vashu x CHAT BOT= Vashu x chatbotdb["Vashu x CHATBOTDb"]["Vashu x CHATBOT"]
+
+
+from .chats import *
+from .users import *
