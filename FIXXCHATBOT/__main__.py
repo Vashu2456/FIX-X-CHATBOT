2ff69a9a8a22a06dcd560ab12fc9e42f4dc0184a
AbhiModszYT <133230865+AbhiModszYT@users.noreply.github.com>
2024-06-08 02:21:00 +0530

    Update __main__.py

--- FIXXCHATBOT/__main__.py
+++ FIXXCHATBOT/__main__.py
@@ -8,16 +8,10 @@ from FIXXCHATBOT.modules import ALL_MODULES
 async def vashu_boot():
     try:
         await app.start()
-    except Exception as ex:
-print(ex)
-        quit(1)
-
     for all_module in ALL_MODULES:
         importlib.import_module("FIXXCHATBOT.modules." + all_module)
 print("bot Started.")
     await idle()
-
-
 if __name__ == "__main__":
     asyncio.get_event_loop().run_until_complete(vashu_boot())
 print("Stopping app Bot vashu...")
