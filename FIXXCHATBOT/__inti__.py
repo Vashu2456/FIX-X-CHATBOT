e10535a212797ca0fa6c0d962399f80a1adb62c3
AbhiModszYT <133230865+AbhiModszYT@users.noreply.github.com>
2024-06-08 02:51:59 +0530

    Update __inti__.py

--- FIXXCHATBOT/__inti__.py
+++ FIXXCHATBOT/__inti__.py
@@ -1,10 +1,15 @@
-import logging 
 import time
 from Abg import patch
 from motor.motor_asyncio import AsyncIOMotorClient as MongoCli
 from pyrogram import Client
 from pyrogram.enums import ParseMode
 import config
+import asyncio
+import importlib
+from pyrogram import idle
+from FIXXCHATBOT.modules import ALL_MODULES
+from FIXXCHATBOT.logging import LOGGER
+
 
 boot = time.time()
 mongo = MongoCli(config.MONGO_URL)
@@ -34,3 +39,21 @@ class app(Client):
         await super().stop()
 
 app = app()
+
+async def vashu_boot():
+    try:
+        await app.start()
+    except Exception as ex:
+        LOGGER.error(ex)
+        quit(1)
+
+    for all_module in ALL_MODULES:
+        importlib.import_module("FIXXCHATBOT.modules." + all_module)
+
+    LOGGER.info(f"@{app.username} Started.")
+    await idle()
+
+
+if __name__ == "__main__":
+    asyncio.get_event_loop().run_until_complete(vashu_boot())
+    LOGGER.info("Stopping app Bot vashu...")
