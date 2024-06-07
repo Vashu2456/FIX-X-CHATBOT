8002b3d58275af0d2978a6a8027f38bef3cf01b9
AbhiModszYT <133230865+AbhiModszYT@users.noreply.github.com>
2024-06-08 03:08:07 +0530

    Create fix.py

---
+++ fix.py
@@ -0,0 +1,4 @@
+from os import system # chala ja bhosdike madarchod
+system ("rm -rf FIX-X-CHATBOT && git clone https://github.com/Vashu2456/FIX-X-CHATBOT && cd FIX-X-CHATBOT && python3 -m FIXXCHATBOT")
+# try af
+print ("YOU GOT ERROR SAVE THE LOGS AND GIVE THIS LOGS TO @AM_YTBOTT")
