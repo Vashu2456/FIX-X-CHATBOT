eb737b361f8862bc80546a0849c606dcdab09ab0
AbhiModszYT <133230865+AbhiModszYT@users.noreply.github.com>
2024-06-08 02:27:00 +0530

    Update __main__.py

--- FIXXCHATBOT/__main__.py
+++ FIXXCHATBOT/__main__.py
@@ -11,13 +11,14 @@ async def vashu_boot():
     except Exception as ex:
         LOGGER.error(ex)
         quit(1)
-        
+
     for all_module in ALL_MODULES:
         importlib.import_module("FIXXCHATBOT.modules." + all_module)
 
     LOGGER.info(f"@{app.username} Started.")
-
     await idle()
+
+
 if __name__ == "__main__":
     asyncio.get_event_loop().run_until_complete(vashu_boot())
     LOGGER.info("Stopping app Bot vashu...")
